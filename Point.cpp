7d19d38eb30c15f3989bb06bd8a6380950149435
Orion Davis <ord4@zips.uakron.edu>
2017-02-07 13:44:50 -0500

    Added Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,13 @@
+/*
+  Orion Davis 3003072 ord4@zips.uakron.edu
+  The University of Akron, Computer Science II
+  ASSGN3: Sophisticated Rectangle
+
+  This is the implementation file for the Point class
+*/
+#include "Point.hpp"
+
+Point::Point(double x, double y){
+   xCoord = x;
+   yCoord = y;
+}
